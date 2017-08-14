d6e9fc47bbfd755adb762c320174d3bdd1669df6
verdantpath <m_dittmann@yahoo.com>
2017-08-13 20:57:36 -0700

    update js

--- battleship.js
+++ battleship.js
@@ -17,12 +17,13 @@ var isSunk = false;
 
 while (isSunk == false) {
   guess = prompt("Ready, aim, fire! (Enter a number from 0-6):");
-  if (guess < 0 || > 6) {
+  if (guess < 0 || guess > 6) {
     alert("Please enter a valid cell number");
   } else {
     guesses = guesses + 1;
     if (guess == location1 || guess == location2 || guess == location3) {
       hits = hits + 1;
+      alert("HIT!");
 
       if (hits == 3) {
         isSunk = true;
