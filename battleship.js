42648c516717d08d1f93b40167dde1e82ef5db61
verdantpath <m_dittmann@yahoo.com>
2017-08-13 20:20:37 -0700

    update js

--- battleship.js
+++ battleship.js
@@ -23,7 +23,12 @@ while (isSunk == false) {
     guesses = guesses + 1;
     if (guess == location1 || guess == location2 || guess == location3) {
       hits = hits + 1;
-      
+
+      if (hits == 3) {
+        isSunk = true;
+        alert("You sunk my battleship!");
+      }
+
     }
   }
 
