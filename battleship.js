5c6ae5051267269f713978e11207e4b3720b8e1a
Matt <m_dittmann@yahoo.com>
2017-10-04 18:22:15 -0700

    update js

--- battleship.js
+++ battleship.js
@@ -1,7 +1,8 @@
 // the placement of the ship
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location1 + 2;
 
 // stores the current guess from the user input
 var guess;
